33deead66a754062769b4098f39830488ad28b9e
duddk <duddk@scatterx.io>
2025-05-09 15:46:05 +0900

    axios 사용해보기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,15 +1,15 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router'
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
   const { isLoading, error, data: videos } = useQuery({
     queryKey: ['videos', keyword],
     queryFn: async () => {
-      return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-              .then(res => res.json())
-              .then(data => data.items);
+      return axios.get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+              .then(res => res.data.items)
     }
   }); 
 
